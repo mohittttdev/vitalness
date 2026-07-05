11c5aeef6cd73487be901819667227c55cf16a69
mohit sharma <mohitttt009@gmail.com>
2026-07-05 21:18:41 +0530

    Refactor code structure for improved readability and maintainability

--- src/script.js
+++ src/script.js
@@ -345,3 +345,34 @@ hamburger.addEventListener("click", () => {
     }
 
 });
+
+
+
+// FOR REVIEW SLIDER
+const swiper = new Swiper(".reviewSwiper", {
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
